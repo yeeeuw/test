951c9e682c509bbde5912ac7e00a393b5de19329
yeeeuw <josua.theron@bpdzenith.com>
2024-08-14 07:21:51 +0400

    u

--- text_to_sql_pipeline.py
+++ text_to_sql_pipeline.py
@@ -40,13 +40,13 @@ class Pipeline:
             **{
                 "pipelines": ["*"],                                                           # Connect to all pipelines
                 "DB_HOST": os.getenv("DB_HOST", "http://localhost"),                     # Database hostname
-                "DB_PORT": os.getenv("DB_PORT", 5432),                                        # Database port 
+                "DB_PORT": os.getenv("DB_PORT", "5432"),                                        # Database port 
                 "DB_USER": os.getenv("DB_USER", "postgres"),                                  # User to connect to the database with
-                "DB_PASSWORD": os.getenv("DB_PASSWORD", "password"),                          # Password to connect to the database with
-                "DB_DATABASE": os.getenv("DB_DATABASE", "postgres"),                          # Database to select on the DB instance
-                "DB_TABLE": os.getenv("DB_TABLE", "table_name"),                            # Table(s) to run queries against 
+                "DB_PASSWORD": os.getenv("DB_PASSWORD", "qwer5678"),                          # Password to connect to the database with
+                "DB_DATABASE": os.getenv("DB_DATABASE", "testdb"),                          # Database to select on the DB instance
+                "DB_TABLE": os.getenv("DB_TABLE", "actor"),                            # Table(s) to run queries against 
                 "OLLAMA_HOST": os.getenv("OLLAMA_HOST", "http://host.docker.internal:11434"), # Make sure to update with the URL of your Ollama host, such as http://localhost:11434 or remote server address
-                "TEXT_TO_SQL_MODEL": os.getenv("TEXT_TO_SQL_MODEL", "llama3.1:latest")            # Model to use for text-to-SQL generation      
+                "TEXT_TO_SQL_MODEL": os.getenv("TEXT_TO_SQL_MODEL", "llama2:latest")            # Model to use for text-to-SQL generation      
             }
         )
 
